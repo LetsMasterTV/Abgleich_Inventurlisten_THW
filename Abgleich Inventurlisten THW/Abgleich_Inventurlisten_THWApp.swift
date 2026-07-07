0c957156bbf33fb56b54f3bcd68c19c9b88437d3
Kai Bühner <sebastian10042002@gmail.com>
2026-07-07 14:58:10 +0200

    Initial Commit

---
+++ Abgleich Inventurlisten THW/Abgleich_Inventurlisten_THWApp.swift
@@ -0,0 +1,17 @@
+//
+//  Abgleich_Inventurlisten_THWApp.swift
+//  Abgleich Inventurlisten THW
+//
+//  Created by Kai Sebastian Bühner on 07.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct Abgleich_Inventurlisten_THWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Abgleich Inventurlisten THW/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Abgleich Inventurlisten THW
+//
+//  Created by Kai Sebastian Bühner on 07.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
